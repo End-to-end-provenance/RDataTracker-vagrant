d5ac954cc62ccaab1153c0d491c2a719edf524c7
tfjmp <thomas.francis.pasquier@gmail.com>
2016-07-07 18:14:55 -0400

    point to EtoE repository rather than alias (never saw github alias expiring, but you never know). Thanks Margo.

--- shared/install.sh
+++ shared/install.sh
@@ -1,4 +1,4 @@
 sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/', dependencies = TRUE)\""
 sudo su - -c "R -e \"install.packages('gtools', repos='http://cran.rstudio.com/', dependencies = TRUE)\""
 sudo su - -c "R -e \"install.packages('plotrix', repos='http://cran.rstudio.com/', dependencies = TRUE)\""
-sudo su - -c "R -e \"devtools::install_github('blernermhc/RDataTracker')\""
+sudo su - -c "R -e \"devtools::install_github('End-to-end-provenance/RDataTracker')\""
